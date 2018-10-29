680ffcb736c395fe4e6ba4f911a9be6f39c2e726
le van thi <thi174hcmus@gmail.com>
2018-10-29 20:39:50 +0700

    fix error when reset page

--- Source/client-web-app/web-app/src/app/services/hoc-sinh.service.ts
+++ Source/client-web-app/web-app/src/app/services/hoc-sinh.service.ts
@@ -1,46 +1,9 @@
-// // Import thư viện
-// import { Injectable } from '@angular/core';
-// import { Http, Response, Headers } from '@angular/http';
-// import { Observable } from 'rxjs/internal/Observable';
-// import { map } from 'rxjs/operators';
-
-// // Import lớp cha
-// import { BaseService } from './base.service';
-
-// // Import đối tượng học sinh
 import { HocSinhObject } from '../object/hoc-sinh-object';
-
-
-// @Injectable()
-// export class HocSinhService extends BaseService {
-//     private headerOptions: any;
-//     protected readonly httpHeader;
-//     private http: Http;
-//     constructor() {
-//         super();
-//         this.headerOptions = {
-//             headers: new Headers({
-//                 'Content-Type': 'application/json'
-//             })
-//         };
-//     }
-
-//     getAllHocSinh(): any {
-//         return this.http.get('http://localhost:56697/rest/hoc-sinh/get-all/');
-//     }
-
-//     getOneHocSinh(id: number): any {
-//     }
-
-// }
-
-
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { catchError } from 'rxjs/operators';
 import { Observable } from 'rxjs';
 
-@Injectable()
+@Injectable({ providedIn: 'root' })
 export class HocSinhService {
     private headerOptions: any;
     protected readonly httpHeader;
