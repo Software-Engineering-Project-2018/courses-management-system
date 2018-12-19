90a73168868e68b71c00bf988adfe87b97d77b7a
le van thi <thi174hcmus@gmail.com>
2018-12-19 16:04:37 +0700

    fix bug 1912

--- Source/client-web-app/web-app/src/app/services/base.service.ts
+++ Source/client-web-app/web-app/src/app/services/base.service.ts
@@ -4,8 +4,8 @@ import { HttpHeaders } from '@angular/common/http';
 
 
 export class BaseService {
-  // public prefixRestUrl = 'http://localhost:50734';
-  public prefixRestUrl = 'https://quanlylophoc.tk';
+  public prefixRestUrl = 'http://localhost:50734';
+  // public prefixRestUrl = 'https://quanlylophoc.tk';
   protected readonly httpHeader;
   public storageService: StorageService;
   constructor(public injector: Injector) {
